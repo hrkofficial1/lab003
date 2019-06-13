898418983df02cdb76934e98d965aee760f06066
fa17-bse-028 <fa17-bse-028@LAB5C-04>
2019-06-13 09:38:08 +0500

    Add project files.

---
+++ LabTask/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LabTask.Startup))]
+namespace LabTask
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
